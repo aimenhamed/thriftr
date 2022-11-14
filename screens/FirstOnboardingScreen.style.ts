d872c747d90fcaeee9e3f99fbbdd30a32b25b439
Oliver Xu <z5312546@ad.unsw.edu.au>
2022-11-15 02:40:49 +1100

    started onboarding

---
+++ screens/FirstOnboardingScreen.style.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    background: {
+        backgroundColor: "#1F1F1F",
+        flex: 1,
+    },
+    viewport: {
+        flex: 1,
+        justifyContent: "center",
+    },    
+    optionButton: {
+        backgroundColor: "white",
+        width: 200,
+        height: 40,
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 1,
+    },
+    submitButton: {
+        backgroundColor: "#FFE500",
+        width: 200,
+        height: 40,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttonText: {
+        fontSize: 16,
+        fontFamily: "AzeretMono_400Regular",
+        color: "black",
+    },
+    normalText: {
+        fontSize: 12,
+        fontFamily: "AzeretMono_400Regular",
+        color: "white",
+    }
+})
+
---
+++ screens/FirstOnboardingScreen.tsx
@@ -0,0 +1,63 @@
+import { View, Text, Button } from "react-native";
+import { PageProps } from "../Router";
+import React, {useState} from 'react';
+import { styles } from "./FirstOnboardingScreen.style";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+type FirstOnboardingScreenProps = {
+  logIn: (userId: string) => void;
+  navigation: () => any;
+} & PageProps;
+
+const FirstOnboardingScreen = ({ navigation }: FirstOnboardingScreenProps) => {
+  const userId = "74aa9a46-aff3-4ecc-a817-f6697b18eb74";
+  return (
+    <View style={styles.background}>
+      <View style={styles.viewport}>
+        <Text style={[styles.normalText, {fontSize: 16}, {paddingLeft: 20}, {paddingBottom: 10}]}>
+          Before we start...
+        </Text>
+        <Text style={[styles.normalText, {fontSize: 16}, {padding: 20}]}>
+          Are you looking for male or female clothes?
+        </Text>
+      </View>
+
+      <View style={[styles.viewport, {alignItems: "center"}, {justifyContent: "space-evenly"}]}>
+        <TouchableOpacity style={styles.optionButton}>
+          <Text style={styles.buttonText}>
+            Male
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.optionButton}>
+          <Text style={styles.buttonText}>
+            Female
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.optionButton}>
+          <Text style={styles.buttonText}>
+            Both
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={[styles.viewport, {alignItems: "center"}]}>
+        <Text style={[styles.normalText, {marginTop: -50}]}>
+          You can change this later
+        </Text>
+        <Text style={styles.normalText}>
+          through your profile
+        </Text>
+        <TouchableOpacity style={[styles.submitButton, {marginTop: 30}]}>
+          <Text style={styles.buttonText}>
+            Next
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
+  );
+};
+
+export default FirstOnboardingScreen;
